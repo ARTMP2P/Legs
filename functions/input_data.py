f2813761fc9cef3ec7f2955add05ce664a7581ab
StarMi0 <starscream2705@gmail.com>
2023-04-10 14:01:18 +0300

    Обновлен test img downloading

--- functions/input_data.py
+++ functions/input_data.py
@@ -1,7 +1,6 @@
 import os
 from keras.models import load_model
 
-current_path = os.path.dirname(os.getcwd())
-print(os.getcwd())
+current_path = os.getcwd()
 RAKURSES = ['0', '35', '90', '145', '180', '215', '270', '325']
 g_model = load_model(os.path.join(current_path, 'data_a', 'models', 'M_1.h5'), compile=True)
