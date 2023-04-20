34f7978291bb3d6a4d5db091aa40bf68ba214690
StarMi0 <starscream2705@gmail.com>
2023-04-20 03:08:32 +0300

    Adding

--- learning/file_load.py
+++ learning/file_load.py
@@ -147,7 +147,7 @@ for d in dir_260_clear:
 
 for d in dir_test:
   list_img_test.append(np.concatenate(list(map(read_img, ((get_list_dir(d))[0]))), axis=-1))
-  list_img_test_25.append(np.concatenate(list(map(read_img25, ((get_list_dir(d))[1]))), axis=-1))
+  list_img_test_25.append(np.concatenate(list(map(read_img, ((get_list_dir(d))[1]))), axis=-1))
 
 print('list_img_test shape=', list_img_test[0].shape, 'list_img_test 25 shape=', list_img_test_25[0].shape)
 
