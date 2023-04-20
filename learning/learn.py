5ddc722b850b72163cd8e26f55563e9c572b9234
StarMi0 <starscream2705@gmail.com>
2023-04-20 16:55:16 +0300

    Adding

--- learning/learn.py
+++ learning/learn.py
@@ -67,6 +67,7 @@ def summarize_performance(step, g_model, f=0):
             IMG = np.concatenate((np.expand_dims(im[:, :, i] * 255, 2),
                                   np.expand_dims(list_img_test_25[j][:, :, i] * 255, 2), np.zeros((SIZE, SIZE, 1))),
                                  axis=-1)
+            print(f"Gan images unique: {np.unique(im[:, :, i])}\nTest image unique: {list_img_test_25[j][:, :, i]}")
             '''
             Вычисляет абсолютную разницу для каждого элемента между двумя массивами или между массивом и скаляром.
             '''
