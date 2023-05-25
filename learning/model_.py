9452740f00d7c844ed1c800edb5fe50f73d66236
StarMi0 <starscream2705@gmail.com>
2023-05-25 14:15:39 +0300

    Optimiset torch, correct Errors

--- learning/model_.py
+++ learning/model_.py
@@ -200,7 +200,7 @@ def define_generator(image_shape):
     """
     # Weight initialization
     init = nn.init.normal_
-
+    print(f"image shape: {image_shape}")
     # Image input
     in_image = torch.zeros(image_shape)
 
