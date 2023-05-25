2b445d017841ddddac2946d4ee2a551fd054e32a
StarMi0 <starscream2705@gmail.com>
2023-05-25 14:49:39 +0300

    Optimiset torch, correct Errors

--- learning/model_.py
+++ learning/model_.py
@@ -155,10 +155,10 @@ def decoder_block(layer_in, skip_in, n_filters, dropout=True):
         Output layer tensor.
     """
     # Weight initialization
-    init = nn.init.normal_
+    init = nn.init.normal_(layer_in)
     print(layer_in.shape[0])
     # Add upsampling layer
-    g = nn.ConvTranspose2d(layer_in.shape[1],
+    g = nn.ConvTranspose2d(init.shape[1],
                            n_filters,
                            kernel_size=4,
                            stride=2,
