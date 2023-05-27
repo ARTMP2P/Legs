e4e5a15491099c432c4c63ef184461d9c8774b48
StarMi0 <starscream2705@gmail.com>
2023-05-27 14:48:00 +0300

    Optimiset torch, correct Errors

--- learning/model_.py
+++ learning/model_.py
@@ -366,6 +366,7 @@ if __name__ == '__main__':
         # Bottleneck, no batch norm and ReLU
         b = nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1, bias=False)
         nn.init.normal_(b.weight, mean=0.0, std=0.02)
+        print(b.shape)
 
         # Add dimension
         b = b(x)
