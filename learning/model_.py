ae5f4165521094100f6c665b0dad8e64bbf54c6c
StarMi0 <starscream2705@gmail.com>
2023-05-27 18:09:49 +0300

    Optimiset torch, correct Errors

--- learning/model_.py
+++ learning/model_.py
@@ -248,7 +248,6 @@ def define_generator(image_shape):
     out_image = g(d7)
     nn.init.normal_(g.weight, mean=0.0, std=0.02)
     out_image = nn.Tanh()(out_image)
-    out_image = torch.nn.Module()
 
     # Define model
     model = nn.Sequential(
