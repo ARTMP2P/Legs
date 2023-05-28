5221a7aa99ff8ab3302d91d94762670a51aaecf1
StarMi0 <starscream2705@gmail.com>
2023-05-29 01:36:48 +0300

    functions changet to torch modules

--- learning/model_.py
+++ learning/model_.py
@@ -109,7 +109,7 @@ class EncoderBlock(nn.Module):
         self.channels = channels
         self.batchnorm = batchnorm
 
-        self.conv2d = nn.Conv2d(self.input_tensor.shape[1], self.channels,
+        elf.conv2d = nn.Conv2d(self.input_tensor.size()[1], self.channels,
                                 kernel_size=3, stride=2, padding=1, bias=False)
         self.batchnorm2d = nn.BatchNorm2d(self.channels)
         self.relu = nn.ReLU(inplace=True)
