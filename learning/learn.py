1e4604910b292db871cbdaecdf6da8fdad958b73
StarMi0 <starscream2705@gmail.com>
2023-06-13 20:57:35 +0300

    fix train

--- learning/learn.py
+++ learning/learn.py
@@ -194,7 +194,7 @@ def summarize_performance(step, generator, dataloader, f=0):
 
         generator.train()
     except Exception as e:
-        print('Error:', str(e), img_diff_resized.shape, generated_img.shape, original_img.shape)
+        print('Error:', str(e), img_diff.shape, generated_img.shape, original_img.shape)
 
 
 def train(generator, dataset, num_epochs, batch_size, patch_shape):
