6309f4e82ef09c11d2d000864153e69f2c9c30b8
StarMi0 <starscream2705@gmail.com>
2023-06-13 17:44:46 +0300

    fix train

--- learning/learn.py
+++ learning/learn.py
@@ -163,6 +163,8 @@ def summarize_performance(step, generator, dataloader, f=0):
         generator.eval()
         with torch.no_grad():
             for j, (inputs, labels) in enumerate(dataloader):
+                inputs = inputs.view(-1, inputs.size(2), inputs.size(3))
+
                 outputs = generator(inputs)
                 percentage_list = []
 
