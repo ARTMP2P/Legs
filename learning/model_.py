c58444ad755f038919df3e457285436f6fb38dbd
StarMi0 <starscream2705@gmail.com>
2023-06-17 02:04:10 +0300

    fix train

--- learning/model_.py
+++ learning/model_.py
@@ -97,10 +97,9 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        # Реализуйте архитектуру вашего дискриминатора
-        # Пример:
+        # Реализация архитектуры дискриминатора
         self.model = nn.Sequential(
-            nn.Conv2d(8 * 2, 64, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(8, 64, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
