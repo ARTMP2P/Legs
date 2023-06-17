98654c4308ad0f5aec41e1f31962ab4e6d3bcebf
StarMi0 <starscream2705@gmail.com>
2023-06-17 05:11:55 +0300

    fix train

--- learning/learn.py
+++ learning/learn.py
@@ -38,9 +38,10 @@ def summarize_performance(step, generator, dataset_list, device, save_model=True
 
         outputs = generator(inputs)
 
-        generated_img = np.where(outputs < 0, 0, outputs)
+        generated_img = outputs.detach().cpu().numpy()[0]
+        generated_img = np.where(generated_img < 0, 0, generated_img)
         generated_img = np.where(generated_img >= 0, 1, generated_img)
-        generated_img = generated_img.astype(np.uint8)[0]
+        generated_img = generated_img.astype(np.uint8)
 
         original_img = labels[0]
 
