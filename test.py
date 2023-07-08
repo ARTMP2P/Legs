69b23457ae8b1fe0664c08d6d27f664beb544e9f
stars <starscream2705@gmail.com>
2023-07-08 17:38:17 +0300

    Correct test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 # Установите путь к файлу архива, который вы хотите отправить
-archive_file_path = '/in/archive.zip'
+archive_file_path = './in/archive.zip'
 
 # URL-адрес API-сервера
 api_url = 'http://localhost:8080/images'
@@ -16,7 +16,7 @@ response = requests.post(api_url, files=files, data=data)
 # Проверка статуса ответа
 if response.status_code == 200:
     # Сохранить полученный архив на диск
-    output_archive_path = '/output/output.zip'
+    output_archive_path = './output/output.zip'
     with open(output_archive_path, 'wb') as output_file:
         output_file.write(response.content)
     print('Обработанный архив сохранен в', output_archive_path)
